f45d420ed494b944236cded87e6aaa8c1926567f
davieoba <bodunrindavidbond@gmail.com>
2023-04-14 08:46:42 +0100

    feat: I have gotten the user data and it is working, thank you master Jesus

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,18 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
+interface User {
+  avatar: Object
+  _id: string
+  name: string
+  email: string
+  password: string
+  role: string
+  passwordChangedAt: string
+  createdAt: string
+  __v: number
+}
+
 export default NextAuth({
   providers: [
     CredentialsProvider({
@@ -44,8 +56,31 @@ export default NextAuth({
       return url
     },
     async jwt({ token, user }) {
-      console.log({ user })
-      return token
+      // console.log({ user })
+      return { token, user }
+    },
+    async session({ session, token, user }) {
+      const userSession = { ...session, accessToken: null, userId: '' }
+      const tokenData: {
+        token: Object
+        user: {
+          success: boolean
+          token: string
+          user: User
+          iat: Date
+          exp: Date
+          jti: string
+        }
+      } = token.token as any
+
+      console.log(tokenData.user.user)
+
+      // session.user
+
+      // console.log(token?.token?.user?.user)
+
+      // userSession['accessToken'] = token
+      return session
     },
   },
 })
