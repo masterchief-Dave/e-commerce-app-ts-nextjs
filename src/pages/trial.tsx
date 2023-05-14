277d74009f2a12adec54bc6178f93e6018608c5e
davieoba <bodunrindavidbond@gmail.com>
2023-05-14 20:19:57 +0100

    feat: build the mobile navbar dropdown

--- src/pages/trial.tsx
+++ src/pages/trial.tsx
@@ -28,8 +28,11 @@ const MobileNavbar = () => {
   const [showMenu, setShowMenu] = useState<Boolean>(false)
 
   const styles = {
-    list: `p-4 font-medium text-white hover:bg-[#fff]/20`,
+    list: `p-4 font-medium text-white hover:bg-[#fff]/20 rounded-xl`,
+    links: `w-full h-full block`,
   }
+
+  // handle the case for click outside of the opened menu or when esc key is pressed on the keyboard
 
   return (
     <nav className='bg-primary-blue-100 py-4'>
@@ -42,26 +45,33 @@ const MobileNavbar = () => {
           </h1>
         </li>
 
-        <li className=''>
+        <li className='cursor-pointer' onClick={() => setShowMenu(!showMenu)}>
           <Bars3Icon className='h-10 w-10 text-white' />
         </li>
-        <div className='absolute top-20 w-[80%] rounded-md bg-[#000] p-10 text-white'>
-          <ul>
-            <li className={styles.list}>
-              <Link href='/profile'>Profile</Link>
-            </li>
-            <li>
-              <input
-                type='text'
-                placeholder='Search'
-                className={`block w-full rounded-md p-4 font-medium text-black`}
-              />
-            </li>
-            <li className={styles.list}>
-              <Link href='/cart'>Cart</Link>
-            </li>
-          </ul>
-        </div>
+
+        {showMenu && (
+          <div className='absolute top-16 left-[10%] mx-auto w-[80%] rounded-xl bg-[#000] p-10 text-white'>
+            <ul className='space-y-4'>
+              <li className={styles.list}>
+                <Link href='/profile' className={styles.links}>
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <input
+                  type='text'
+                  placeholder='Search'
+                  className={`block w-full rounded-md p-4 font-medium text-black`}
+                />
+              </li>
+              <li className={styles.list}>
+                <Link href='/cart' className={styles.links}>
+                  Cart
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </ul>
     </nav>
   )
