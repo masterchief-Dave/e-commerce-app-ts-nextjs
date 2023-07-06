0dedf03553f13a13916c12e770f9853529f22ef3
davieoba <bodunrindavidbond@gmail.com>
2023-07-07 00:29:36 +0100

    chec before accessing the window object

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,3 +1,4 @@
+'use client'
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '@/features/cart/cartSlice'
 import wishListReducer from '@/features/wishlist/wishlistSlice'
@@ -5,6 +6,18 @@ import loginReducer from '@/features/login/loginSlice'
 import Cookies from 'js-cookie'
 
 const authToken = Cookies.get('authLoginToken');
+// chec if the user is in the localstorage
+let savedUser
+if (typeof window !== 'undefined') {
+  savedUser = window.localStorage.getItem('user')
+}
+
+const initialState = {
+  isLoggedIn: !!savedUser,
+  user: savedUser ? JSON.parse(savedUser) : null,
+  error: null,
+  loading: false
+}
 
 export const store = configureStore({
   reducer: {
@@ -12,9 +25,22 @@ export const store = configureStore({
     wishlist: wishListReducer,
     login: loginReducer
   },
+  preloadedState: {
+    login: initialState
+  }
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+/**
+ * const initialState = {
+  isLoggedIn: !!savedUser, // Set isLoggedIn to true if user data exists
+  user: savedUser ? JSON.parse(savedUser) : null,
+  error: null,
+};
+
+ preloadedState: { login: initialState }, // Set the initial state
+ */
