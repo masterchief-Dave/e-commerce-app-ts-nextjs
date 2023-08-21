7b939f4e1c55af74378b89c30f19899c98c7878a
davieoba <bodunrindavidbond@gmail.com>
2023-08-21 06:20:33 +0100

    feat: add paystack payment api

---
+++ src/helpers/paystack.js
@@ -0,0 +1,47 @@
+import { useRouter } from 'next/router'
+import { usePaystackPayment } from 'react-paystack'
+
+// type Props = {
+//   loading: boolean
+//   orders: Product[]
+//   price: string
+// }
+
+const config = {
+  reference: new Date().getTime().toString(),
+  email: 'bodunrindavidbond@gmail.com',
+  amount: 20000,
+  publicKey: process.env.NEXT_PUBLIC_PAYSTACK_API,
+  // currency: 'USD',
+}
+
+const onClose = () => {
+  // implementation for  whatever you want to do when the Paystack dialog closed.
+  console.log('closed')
+}
+
+export const PaystackHook = ({ loading, orders, price }) => {
+  const router = useRouter()
+  const amountToPay = parseFloat(price) * 100
+
+  const onSuccess = (reference) => {
+    // Implementation for whatever you want to do with reference and after success call.
+    console.log(reference)
+    router.push({
+      pathname: '/success',
+      query: { orders, reference, price: amountToPay },
+    })
+  }
+
+
+  const data = { ...config, amount: amountToPay }
+  const initializePayment = usePaystackPayment(data)
+
+  return (
+    <button
+      className='h-[4rem] bg-blue-500 text-white text-[1.4rem] font-medium rounded-md px-8 flex items-center justify-center w-fit'
+      onClick={() => initializePayment(onSuccess, onclose)}>
+      Check out
+    </button>
+  )
+}
--- src/pages/demos/demo.jsx
+++ src/pages/demos/demo.jsx
@@ -3,8 +3,10 @@ import { NavigationMenuDemo } from '@/components/Dropdown/NavigationDropdownMenu
 import ResetEmailSentModal from '@/components/Modal/ResetEmailSent'
 import { Button } from '@chakra-ui/react'
 import axios from 'axios'
+import { PaystackHook } from '@/helpers/paystack'
 
 const NavigationMenuDemoExample = () => {
+
   const fn = async () => {
     try {
       const response = await axios.post('/api/products/create-product')
@@ -14,11 +16,14 @@ const NavigationMenuDemoExample = () => {
       console.log(err)
     }
   }
+
+  console.log(process.env.NEXT_PUBLIC_PAYSTACK_API)
   return (
     // <NavigationMenuDemo />
     <div className='flex p-24'>
-      <Button variant={'solid'} onClick={() => fn()}>simulate post request</Button>
+      {/* <Button variant={'solid'} onClick={() => fn()}>simulate post request</Button> */}
       {/* <ResetEmailSentModal /> */}
+      <PaystackHook price={20000} />
     </div>
   )
 }
