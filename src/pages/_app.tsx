171402498082247883cb87055471d2b7d4e9a156
davieoba <bodunrindavidbond@gmail.com>
2023-09-02 20:56:28 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,14 @@ import 'slick-carousel/slick/slick-theme.css'
 import './demos/demo.scss'
 import '../components/Dropdown/dropdown.scss'
 import '@/styles/main.scss'
+
 import { Toaster } from 'react-hot-toast'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
-
-import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import type { AppProps } from 'next/app'
 import type { Session } from 'next-auth'
+
 import { store } from '@/app/store'
 
 export default function App({
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* <script src="//js.paystack.co/v1/inline.js"></script> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'use-credentials'} />
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
