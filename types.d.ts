4e27f17fdfce46e86a006d886abb811199d5409c
davieoba <bodunrindavidbond@gmail.com>
2023-09-02 14:31:13 +0100

    update the types

--- types.d.ts
+++ types.d.ts
@@ -1,216 +1,245 @@
 import { Types } from "mongoose"
 
-interface Header {
-  id: number
-  title: string
-  nameBold: string
-  nameNormal: string
-  description: string
-  price: string
-  formerPrice: string
-  promoPrice: string
-  img: JSX.Element | string | StaticImageData
-}
 
-interface BestDeals {
-  id: number
-  name: string
-  img: string | StaticImageData
-  rating: number
-  price: number
-  formerPrice: number
+declare global { 
+  interface Header {
+    id: number
+    title: string
+    nameBold: string
+    nameNormal: string
+    description: string
+    price: string
+    formerPrice: string
+    promoPrice: string
+    img: JSX.Element | string | StaticImageData
+  }
 }
-
-interface ICategory {
-  img: string | StaticImageData
-  name: string
-}
-
-interface Product {
-  _id: string
-  name: string
-  price: number
-  description: string
-  ratings: number
-  images: {
-    public_id: string
-    url: string
-    id: string
-  }[]
-  category: string
-  seller: string
-  stock: number
-  numOfReviews: number
-  reviews: {
+declare global {
+  interface BestDeals {
+    id: number
     name: string
+    img: string | StaticImageData
     rating: number
-    comment: string
-  }[]
-  user: string
-  createdAt: Date
+    price: number
+    formerPrice: number
+  }
 }
 
-interface Cart extends Product {
-  cartQuantity: number
+declare global { 
+  interface ICategory {
+    img: string | StaticImageData
+    name: string
+  }
 }
-
-interface Wishlist extends Product {
-  cartQuantity: number
-}
-
-interface PaymentFormInput {
-  userName: string
-  cardNumber: string
-  expiryDate: string
-  cvc: string
-}
-
-interface IDelivery {
-  street: string
-  zipCode: string
-  city: string
-  state: string
-}
-
-interface IAddress extends IDelivery {
-  phoneNumber: string
-  country: string
-  user: Types.ObjectId
-  zipCode: string
-}
-
-interface ICard {
-  name: string
-  cardNumber: string
-  cvc: string
-  expiryDate: Date
-}
-
-
-interface IUser {
-  id: string
-  _id: Types.ObjectId
-  name: string
-  googleId: string
-  email: string
-  // password: string
-  avatar: string,
-  role: string
-  createdAt: Date
-  resetPasswordToken: string
-  resetPasswordExpire: Date
-  passwordChangedAt: Date
-  active: boolean
-  refreshToken: string
-  deliveryAddress: IDelivery[]
-  creditCards: ICard[]
-
-  address: string
-  image: string
-  email: string
-  iat: number
-  exp: number
-  jti: string
-  password: string
-  picture: string
-  role: string
-  sub: string
-  success: boolean
-  token: string
-  user: string
-}
-
-interface User {
-  success: boolean
-  token: string | null
-  _id?: string | Types.ObjectId
-  user: {
-    avatar: {
-      public_id: string
-      url: string
-    }
+declare global {
+  interface Product {
     _id: string
     name: string
-    email: string
-    password: string,
-    role: string
-    passwordChangedAt: Date
-    createdAt: Date
-    __v?: number
-  }
-}
-
-type UserLoginSession = {
-  id: string
-  photo: string
-  name: string
-  email: string
-  success: boolean
-  token: string | null
-}
-
-type UserSession = {
-  id: string
-  photo: string
-  name: string
-  email: string
-  success: boolean
-  token: string | null
-}
-
-type RegisterSession = {
-  id: string
-  photo: string
-  name: string
-  email: string
-  success: boolean
-  token: string | null
-}
-
-interface BillingAddress {
-  title: string
-  firstname: string
-  lastname: string
-  country: string
-  zipcode: string
-  addressLine1: string
-  addressLine2: string
-  default?: Boolean
-}
-
-interface ShippingAddress {
-  street: string
-  city: string
-  state: string
-  phoneNumber: string
-  zipCode: string
-  country: string
-  user: User
-}
-
-interface IOrder {
-  id?: Types.ObjectId
-  cartQuantity?: number
-  shippingInfo: BillingAddress
-  user: User
-  orderItems: {
-    quantity: number
     price: number
-    product: Product
-  }[]
-  paymentInfo: {
-    status: string
-    reference: string
-    message: string
-    transaction: string
+    description: string
+    ratings: number
+    images: {
+      public_id: string
+      url: string
+      id: string
+    }[]
+    category: string
+    seller: string
+    stock: number
+    numOfReviews: number
+    reviews: {
+      name: string
+      rating: number
+      comment: string
+    }[]
+    user: string
+    createdAt: Date
   }
-  taxPrice: number
-  shippingPrice: number
-  totalPrice: number
-  paidAt: Date
-  orderStatus: string
-  deliveredAt: Date
-  createdAt: Date
+}
+declare global {
+  interface Cart extends Product {
+    cartQuantity: number
+  }
+}
+declare global {
+  interface Wishlist extends Product {
+    cartQuantity: number
+  }
+}
+declare global {
+  interface PaymentFormInput {
+    userName: string
+    cardNumber: string
+    expiryDate: string
+    cvc: string
+  }
+}
+declare global {
+  interface IDelivery {
+    street: string
+    zipCode: string
+    city: string
+    state: string
+  }
+}
+declare global {
+  interface IAddress extends IDelivery {
+    phoneNumber: string
+    country: string
+    user: Types.ObjectId
+    zipCode: string
+  }
+}
+declare global {
+  interface ICard {
+    name: string
+    cardNumber: string
+    cvc: string
+    expiryDate: Date
+  }
+}
+
+
+declare global {
+  interface IUser {
+    id: string
+    _id: Types.ObjectId
+    name: string
+    googleId: string
+    email: string
+    // password: string
+    avatar: string,
+    role: string
+    createdAt: Date
+    resetPasswordToken: string
+    resetPasswordExpire: Date
+    passwordChangedAt: Date
+    active: boolean
+    refreshToken: string
+    deliveryAddress: IDelivery[]
+    creditCards: ICard[]
+
+    address: string
+    image: string
+    email: string
+    iat: number
+    exp: number
+    jti: string
+    password: string
+    picture: string
+    role: string
+    sub: string
+    success: boolean
+    token: string
+    user: string
+  }
+}
+
+declare global {
+  interface User {
+    success: boolean
+    token: string | null
+    _id?: string | Types.ObjectId
+    user: {
+      avatar: {
+        public_id: string
+        url: string
+      }
+      _id: string
+      name: string
+      email: string
+      password: string,
+      role: string
+      passwordChangedAt: Date
+      createdAt: Date
+      __v?: number
+    }
+  }
+}
+
+declare global {
+  type UserLoginSession = {
+    id: string
+    photo: string
+    name: string
+    email: string
+    success: boolean
+    token: string | null
+  }
+}
+
+declare global {
+  type UserSession = {
+    id: string
+    photo: string
+    name: string
+    email: string
+    success: boolean
+    token: string | null
+  }
+}
+
+declare global {
+  type RegisterSession = {
+    id: string
+    photo: string
+    name: string
+    email: string
+    success: boolean
+    token: string | null
+  }
+}
+
+declare global {
+  interface BillingAddress {
+    title: string
+    firstname: string
+    lastname: string
+    country: string
+    zipcode: string
+    addressLine1: string
+    addressLine2: string
+    default?: Boolean
+  }
+}
+
+declare global {
+  interface ShippingAddress {
+    street: string
+    city: string
+    state: string
+    phoneNumber: string
+    zipCode: string
+    country: string
+    user: User
+  }
+}
+
+declare global {
+  interface IOrder {
+    id?: Types.ObjectId
+    cartQuantity?: number
+    shippingInfo: BillingAddress
+    user: User
+    orderItems: {
+      quantity: number
+      price: number
+      product: Product
+    }[]
+    paymentInfo: {
+      status: string
+      reference: string
+      message: string
+      transaction: string
+    }
+    taxPrice: number
+    shippingPrice: number
+    totalPrice: number
+    paidAt: Date
+    orderStatus: string
+    deliveredAt: Date
+    createdAt: Date
+  }
 }
 
 /**
